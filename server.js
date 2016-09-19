df4343af414cc382ce1f9ce5d2504d7e0d50ea9e
kkindorf <kevin.kindorf@gmail.com>
2016-09-19 23:21:56 +0000

    added access control allow origin in server

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ var SaveCard = require("./model/saveCard");
 
 var app = express();
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 var app = express();
 var runServer = function(callback){
        console.log(config.DATABASE_URL)
